28e12de10a87857253590ef7f7258a6702146a65
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2023-08-14 13:25:37 +0200

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,42 +1,42 @@
 #!/usr/bin/env python3
-""" 3. Auth class
+"""
+Auth class
 """
 
+from tabnanny import check
 from flask import request
-from typing import List, TypeVar
+from typing import TypeVar, List
+User = TypeVar('User')
 
 
 class Auth:
-    """ Auth class.
+    """
+    a class to manage the API authentication
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth method
         """
-        if path is None or not excluded_paths:
+        returns False - path and excluded_paths
+        """
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        path = path + '/' if path[-1] != '/' else path
-        has_wildcard = any(x.endswith("*") for x in excluded_paths)
-        if not has_wildcard:
-            return path not in excluded_paths
-        for entry in excluded_paths:
-            if entry.endswith("*"):
-                if path.startswith(entry[:-1]):
-                    return False
-            if path == entry:
-                return False
+        if path[-1] != "/":
+            check += "/"
+        if check in excluded_paths or path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header method
+        """
+        returns None - request
         """
         if request is None:
             return None
-        if 'Authorization' not in request.headers:
-            return None
-        return request.headers.get('Authorization')
+        return request.headers.get("Authorization")
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ curent_user method
+    def current_user(self, request=None) -> User:
+        """
+        returns None - request
         """
         return None
